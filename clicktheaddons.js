9ba1b181922942158b00a3e177ade6a42197e028
MCJack123 <jackbruienne@yahoo.com>
2015-12-15 14:31:52 -0500

    Update clicktheaddons.js

--- clicktheaddons.js
+++ clicktheaddons.js
@@ -52,7 +52,8 @@ else {alert("You cannot sell 0 " + name + "s for faces."); return false; }
 // Example addon
 function createSampleAddon(buy) {if (score < 0) brokenGame(); newAddon("Clicker", 15, false, 3, 5); if (buy) buyAddon("Clicker"); }
 function loadConfig() {
- var incode = <?php $fileo = fopen("ctf-config.csv", "r"); echo fread($fileo, filesize("ctf-config.csv")); fclose($fileo); ?>;
+ var incode = document.getElementById("php-interface").innerHTML;
+ while (incode == "") incode = document.getElementById("php-interface").innerHTML;
  incode.split("|");
  var i = 1;
  while (i <= incode.length) {
