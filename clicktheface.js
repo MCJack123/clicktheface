ab5ea0fd4f5acd81ec8baae61b6a83702ba2ae10
MCJack123 <jackbruienne@yahoo.com>
2016-11-08 16:37:36 -0500

    Update clicktheface.js

--- clicktheface.js
+++ clicktheface.js
@@ -1,5 +1,6 @@
 var score = 0;
-var audio = new Audio('onClickSound.mp3');
+var enableAudio = true;
+if (enableAudio) var audio = new Audio('onClickSound.mp3');
 gblScoreMax = -1;
 // Game function
 function doClick(scoremax) {  // When you click the picture
@@ -8,26 +9,27 @@ function doClick(scoremax) {  // When you click the picture
   score = score + 3;
   if (score > scoremax) document.write("Why are you still clicking?");
   document.getElementById("scoretext").innerHTML = score;
-  audio.play();
+  if (enableAudio) audio.play();
   if (score < 0) brokenGame();
 }
 // Secrets
 function youWin() {  // Secret: Win the game
-  var overkill = new Audio('winSound.mp3');
-  overkill.play();
+  if (enableAudio) var overkill = new Audio('winSound.mp3');
+  if (enableAudio) overkill.play();
   score = score + 10000000000;
   document.write("you win! perfect!<br>");
   document.write("With a score of: " + score);
 }
 function youLose() {  // Secret: Get to 1000 points
   score = gblScoreMax - 1;
-  audio.play();
+  if (enableAudio) audio.play();
   document.getElementById("scoretext").innerHTML = score;
 }
 // Initializer
-function initAll(picname, scorename, width) {
+function initAll(picname, scorename, width, doaud) {
   scorename = typeof scorename !== 'undefined' ? scorename : 'Score: ';
   width = typeof width !== 'undefined' ? width : 200;
+  enableAudio = typeof doaud !== 'undefined' ? doaud : true;
   var x = document.createElement("IMG");
   x.setAttribute("src", picname);
   x.setAttribute("width", width);
